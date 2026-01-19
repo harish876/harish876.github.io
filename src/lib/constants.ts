73b4c6a4b906c364880dbabe236644d1213b14a2
harish876 <harishgokul01@gmail.com>
2026-01-18 21:11:54 -0800

    update the original portfolio

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -14,7 +14,4 @@ export function getBasePath(): string {
 }
 
 export const baseUrl =
-  process.env.NEXT_PUBLIC_SITE_URL ??
-    process.env.BASE_PATH
-    ? `https://harish876.github.io${process.env.BASE_PATH}`
-    : "https://harish876.github.io";
+  process.env.NEXT_PUBLIC_SITE_URL ?? "https://harish876.github.io";
